fc0213ceab02140e475e11cc88c25160636e6bd2
LucasBS8 <lucasgithub8@gmail.com>
2024-06-16 14:44:17 -0300

    aula 12

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,47 +1,15 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import List from './components/List'
-import Evento from './components/evento/Evento'
-import Form from './components/Form'
+import Condicional from './Condicional';
 
 function App() {
-    const nome = 'Lucas';
-    const newNome = nome.toLocaleUpperCase();
-    const url = 'https://via.placeholder.com/150';
-
-    function sum(a, b) {
-        return a + b;
-    }
 
     return (
         // Chama "class" como "className" em react
         <div className="App">
-            <h2>
-                Alterando o JSX
-            </h2>
-            <p>Olá, {newNome}</p>
-            <p>Soma: {sum(2, 5)}</p>
-            <img src={url} alt="Minha imagem" />
-            <HelloWorld />
-
-            <SayMyName nome="Matheus" />
-            <SayMyName nome="João" />
-            <SayMyName nome={nome} />
-            <Pessoa nome="Lucas" idade="19" profissao="Programador" foto="https://via.placeholder.com/150" />
-            <List />
-
-            <div>
-                <Evento />
-            </div>
-
-            <div>
-                <Form />
-            </div>
+            <h1>Renderização condicional</h1>
+            <Condicional />
 
         </div>
-
 
     );
 }
